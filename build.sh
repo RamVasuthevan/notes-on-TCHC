749d13983082858a9e0db196caf497bd4fae9041
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-09-13 05:14:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
 /usr/bin/pandoc index.markdown --standalone --from=markdown --to=html --include-in-header=header.html --output=index.html --css=style.css
