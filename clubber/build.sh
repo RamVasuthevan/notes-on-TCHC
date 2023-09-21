a74b7b206487920bf8d6b9415bd4deb95b33a23a
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-09-21 00:44:43 +0000

    Move to template

--- clubber/build.sh
+++ clubber/build.sh
@@ -9,4 +9,4 @@ cd ..
 echo $(pwd)
 
 # Now, run pandoc with the adjusted paths
-/usr/bin/pandoc website/index.markdown --standalone --from=markdown --to=html --include-in-header=website/header.html --output=website/index.html --css=style.css
+/usr/bin/pandoc website/index.markdown --standalone --from=markdown --to=html --include-in-header=website/template/header.html --output=website/index.html --css=website/template/style.css
