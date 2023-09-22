f445d5f0a52ec136ea4cae04461fb3a105bc77b2
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-09-22 16:46:56 +0000

    Add a homepage

--- clubber/build.sh
+++ clubber/build.sh
@@ -11,8 +11,12 @@ echo $(pwd)
 # Ensure _site directory exists
 mkdir -p website/_site
 
-# Run pandoc with the adjusted paths
-/usr/bin/pandoc website/index.markdown --standalone --from=markdown --to=html --include-in-header=website/template/header.html --output=website/_site/index.html --css=template/style.css
-
 # Copy style.css to the _site directory
 cp website/template/style.css website/_site/
+
+# Run pandoc for each markdown file in the website directory
+for file in website/*.markdown; do
+    filename=$(basename -- "$file")
+    base="${filename%.*}"
+    /usr/bin/pandoc "$file" --standalone --from=markdown --to=html --include-in-header=website/template/header.html --output="website/_site/$base.html" --css=style.css
+done
